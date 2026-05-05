fbf06dba8f8c1296b62b78facc42b1b82ef59b47
rendi-09 <88842262+rendi-09@users.noreply.github.com>
2026-05-05 23:16:39 +0700

    Refactor data structure for anggota information

--- data.js
+++ data.js
@@ -3,9 +3,10 @@
 // Pastikan ID di sebelah kiri sama persis dengan isi Barcode/QR Code
 
 const dataAnggota = {
-    "RW-001": "Ahmad Budiarto",
-    "RW-002": "Siti Aisyah",
-    "RW-003": "Dian Sastrowardoyo",
-    "RW-004": "Reza Rahadian",
-    // Tambahkan anggota lain di sini...
+    "RW-001": { nama: "Rudi Yanto", ket: "Perlengkapan" },
+    "RW-002": { nama: "Siti Aisyah", ket: "" }, // Kosongan
+    "RW-003": { nama: "Ahmad Budi", ket: "Divisi Konsumsi" },
+    "RW-004": { nama: "Reza Rahadian", ket: "" },
+    "RW-005": { nama: "Budi Aisyah", ket: "Divisi Konsumsi" },
+    "RW-006": { nama: "Jojon Santoso", ket: "Divisi Pubdok" },
 };
